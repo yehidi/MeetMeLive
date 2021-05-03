f21811958af140d744668df8841a679ce9b03eba
IgalRach <iga_rahman@yahoo.com>
2021-05-03 16:04:54 +0300

    Added attributes to User model moved ChatMessage to Model directory

--- app/src/main/java/com/example/meetmelive/model/User.java
+++ app/src/main/java/com/example/meetmelive/model/User.java
@@ -1,34 +1,58 @@
 package com.example.meetmelive.model;
 
+import java.util.Date;
+import java.util.List;
 public class User {
 
     // static variable single_instance of type Singleton
-    private static User theUser = null;
-
-    public String userUsername;
-    public String userEmail;
-    public String profileImageUrl;
-    public String userId;
-    public String userInfo;
-    public String gender;
+    private static User MyUser = null;
+    //@PrimaryKey
+    //@NonNull
+    public String id = "";
+    public String name = "";
+    public String description="";
+    public Date birthday;
+    public String gender="";
+    public String interestedIn ="";
+    public String currentLocation="";
+    public String email = "";
+    long lastUpdated;
+    public String password;
+    public String city;
+    List<String> listNotification;
+    List<ChatMessage> listFriends;
+    public String profilePic;
+    public String pic1;
+    public String pic2;
+    public String pic3;
 
     private User()
     {
-        userEmail = null;
-        userUsername = null;
-        profileImageUrl = null;
-        userId = null;
-        userInfo = null;
-        gender = null;
+        email = null;
+        name = null;
+        password = null;
+        city = null;
+        id = null;
+        description=null;
+        birthday=null;
+        gender=null;
+        interestedIn =null;
+        currentLocation=null;
+        listNotification=null;
+        listFriends=null;
+        profilePic=null;
+        pic1=null;
+        pic2=null;
+        pic3=null;
     }
 
     // create instance of Singleton class
     public static User getInstance()
     {
-        if (theUser == null)
-            theUser = new User();
+        if (MyUser == null)
+            MyUser = new User();
 
-        return theUser;
+        return MyUser;
     }
 
 }
