53e1ea52dd87dfa338ac179e2a797cba89880b42
Omri <yehidi@gmail.com>
2021-06-03 03:20:55 +0300

    a

--- app/src/main/java/com/example/meetmelive/authentication/register.java
+++ app/src/main/java/com/example/meetmelive/authentication/register.java
@@ -91,7 +91,7 @@ public class register extends AppCompatActivity implements RadioGroup.OnCheckedC
                 Utils.chooseImageFromGallery(register.this);
             }
         });
-
+//a
         choosePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
